6711f951816ca5e544da70d4af003a728f5b0fca
IKEDA Soji <ikeda@conversion.co.jp>
2018-02-20 15:09:13 +0900

    Updating copyright notices.

--- sockstr.h
+++ sockstr.h
@@ -1,11 +1,8 @@
 /* $Id$ */
 /*
- * Sympa - SYsteme de Multi-Postage Automatique
+ * smtpc - SMTP / LMTP client
  *
- * Copyright (c) 1997, 1998, 1999 Institut Pasteur & Christophe Wolfhugel
- * Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
- * 2006, 2007, 2008, 2009, 2010, 2011 Comite Reseau des Universites
- * Copyright (c) 2011, 2012, 2013, 2014, 2015, 2016, 2017 GIP RENATER
+ * Copyright 2015 IKEDA Soji
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
